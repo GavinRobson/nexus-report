952c07cc5cefa642623c3db8698278d8b928d452
GavinRobson <gtrobson3@gmail.com>
2024-09-05 12:36:52 -0400

    account-nav-button changes

--- app/(home)/account/[riotAccountId]/page.tsx
+++ app/(home)/account/[riotAccountId]/page.tsx
@@ -1,7 +1,26 @@
-const AccountPage = () => {
+import { getMatchIdsByPuuid, getProfileIconById } from "@/data/riot";
+import { getRiotAccountById } from "@/data/riotAccounts";
+import Image from "next/image";
+
+interface AccountPageProps {
+  params: {
+    riotAccountId: string;
+  }
+}
+
+const AccountPage = async ({params}: AccountPageProps) => {
+  const { riotAccountId } = params;
+  const riotAccount = await getRiotAccountById(riotAccountId);
+
+  if (!riotAccount) return null;
+
+  const matchIds = await getMatchIdsByPuuid(riotAccount.puuid)
+  const profileIcon = await getProfileIconById(riotAccount.profileIconId);
   return ( 
-    <div>
-      Account Page
+     <div className="flex flex-row justify-between p-10 w-full space-x-2">
+      {matchIds.map((matchId: string) => {
+        return <p>{matchId}</p>
+      })} 
     </div>
    );
 }
